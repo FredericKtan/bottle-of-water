2ff1eeab1646d5238f37fc434409264fac500165
Frederic MAMATH <fredericmamath@gmail.com>
2019-08-12 10:03:11 +0200

    :construction: Clean script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,9 +7,9 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const diff = execSync('git --no-pager  log origin/master..origin/staging --oneline --grep "Merge pull request"', {
-  encoding: 'utf-8',
-});
+const options = { encoding: 'utf-8', stdio : 'pipe' };
+
+const diff = execSync('git --no-pager  log origin/master..origin/staging --oneline --grep "Merge pull request"', options);
 
 const issuesId = compose(map(compose(replace('#', ''), trim)), match(/\s#(.\d*)\s/gm))(diff);
 
@@ -18,9 +18,7 @@ let body = "";
 const issuesTitle = [];
 
 issuesId.map(id => {
-  const issue = execSync(`curl -H "Authorization: token ${process.env.GITHUB_TOKEN}" -H "Content-Type: application/json" https://api.github.com/repos/FredericKtan/bottle-of-water/issues/${id}`, {
-    encoding: 'utf-8',
-  });
+  const issue = execSync(`curl -H "Authorization: token ${process.env.GITHUB_TOKEN}" -H "Content-Type: application/json" https://api.github.com/repos/FredericKtan/bottle-of-water/issues/${id}`, options);
   const jsonParsedIssue = JSON.parse(issue);
 
   issuesTitle.push(jsonParsedIssue.title);
@@ -28,7 +26,7 @@ issuesId.map(id => {
 
 const pullRequest = {
   title: `Push to production - ${process.env.npm_package_version}`,
-  body: '#Push to production - 0.1.0\\n## Changelog\\n',
+  body: `# Push to production - ${process.env.npm_package_version}\\n## Changelog\\n`,
 };
 console.log(`> Found ${issuesId.length} issues`);
 
@@ -37,9 +35,11 @@ issuesTitle.forEach(title => {
   console.log('> - ', title);
 })
 
-console.log({ pullRequest });
-console.log(`curl -H "Authorization: token ${process.env.GITHUB_TOKEN}" -d  '{"title": "${pullRequest.title}", "body": "${pullRequest.body}", "head": "staging", "base": "master"}' https://api.github.com/repos/fredericktan/bottle-of-water/pulls`);
+const createPullRequest = execSync(`curl -H "Authorization: token ${process.env.GITHUB_TOKEN}" -d  '{"title": "${pullRequest.title}", "body": "${pullRequest.body}", "head": "staging", "base": "master"}' https://api.github.com/repos/fredericktan/bottle-of-water/pulls`, options);
 
-const createPullRequest = execSync(`curl -H "Authorization: token ${process.env.GITHUB_TOKEN}" -d  '{"title": "${pullRequest.title}", "body": "${pullRequest.body}", "head": "staging", "base": "master"}' https://api.github.com/repos/fredericktan/bottle-of-water/pulls`, {
-  encoding: 'utf-8',
-});
+const jsonParsedCreatePullRequest = JSON.parse(createPullRequest);
+if (jsonParsedCreatePullRequest.errors) {
+  console.log('> Create pull request failed...\n', jsonParsedCreatePullRequest.errors);
+} else {
+  console.log(`> Pull request created successfully @ ${jsonParsedCreatePullRequest.html_url} !`);
+}
